f3843645a427435da2ac470dc4508ae83f59128e
LautaroVarela <lautaroxvarela@gmail.com>
2020-03-08 21:44:02 -0300

    Agregando mapeo avanzado con JPA, uso de @ManyToOne

--- tricko/src/main/java/com/proyectoSpring/tricko/TrickoApplication.java
+++ tricko/src/main/java/com/proyectoSpring/tricko/TrickoApplication.java
@@ -2,14 +2,13 @@ package com.proyectoSpring.tricko;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.proyectoSpring"})
 public class TrickoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TrickoApplication.class, args);
-	}
+            
+        }
 
 }
--- tricko/src/main/resources/schema-mariadb.sql
+++ tricko/src/main/resources/schema-mariadb.sql
@@ -1,14 +1,41 @@
+drop table if exists obras;
+drop table if exists venta_online;
+
 create table obras (
 id int primary key auto_increment,
 nombre varchar(200) not null,
-descripcion varchar(3000),
+descripcion varchar(longtext),
 imagen_url varchar(500)
 );
 
 insert into obras
-(nombre, descripcion, imagen_url) values
-('Clowns', 'Collage manual con rostros, editado con Photoshop', 'images/01.jpg' ),
-('Hands 02', 'Collage manual compuesto, editado con Photoshop', 'images/03.jpg' ),
-('Monstruos', 'Collage manual y retocado gráfico con Photoshop', 'images/07.jpg' ),
-('Miedo', 'Collage manual y retocado gráfico con Photoshop', 'images/08.jpg' );
+(id, nombre, descripcion, imagen_url) values
+(1, 'Clowns', 'Collage manual con rostros, editado con Photoshop', 'images/01.jpg' ),
+(2, 'Hands 02', 'Collage manual compuesto, editado con Photoshop', 'images/03.jpg' ),
+(3, 'Monstruos', 'Collage manual y retocado gráfico con Photoshop', 'images/07.jpg' ),
+(4, 'Miedo', 'Collage manual y retocado gráfico con Photoshop', 'images/08.jpg' ),
+(5, 'Nosferatu a', 'Poster A1 inspirado en "Nosferatu" y aplicado en una campaña ficticia de teatro', 'images/09.jpg' ),
+(6, 'Nosferatu b', 'Poster A2 inspirado en "Nosferatu" y aplicado en una campaña ficticia de teatro', 'images/10.jpg' );
 
+create table venta_online (
+    id int primary key auto_increment,
+    nombre varchar(200) not null,
+    sitio_web varchar(500)
+);
+
+insert into venta_online 
+(id, nombre, sitio_web) values
+(1, 'Creary', 'creary.net'),
+(2, 'Devian Art', 'devianart.com'),
+(3, 'Red Bubble', 'redbubble.com');
+
+alter table obras
+add column venta_online_id int,
+add foreign key (venta_online_id) references venta_online(id);
+
+update obras set venta_online_id = 1 where id in (1, 3);
+update obras set venta_online_id = 2 where id in (4, 5);
+update obras set venta_online_id = 3 where id in (2, 6);
+
+alter table obras
+modify venta_online_id int not null;
